8545c03f61ca5559747cbb3372c3f94963fd362c
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-11-30 16:34:16 -0600

    Added response message

--- crittermail.php
+++ crittermail.php
@@ -1,55 +1,76 @@
 <?php
+    $response = array("status" => "bad");
 
-if(isset($_POST['email'])){
+if(!isset($_POST['email']) || !isset($_POST['password']))) {
+    $response['message'] = "Missing Input!";
     
-    require '/home/robert/config/conn.php';
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    
-    $email = htmlspecialchars($_POST['email']);
-    $password = htmlspecialchars($_POST['password']);
-    $salt = htmlspecialchars($_POST['salt']);
-    $activation = strtoupper(substr(hash("sha256",time(),false),0,5));
-    
-    
-    $sql="select * from user where email = '" . $email . "';";
-    
-    //echo $sql;
-    $result = mysqli_query($mysqli,$sql);
-    
-    //echo mysql_error();
-    if (!$result) {
-        die('Invalid query: ' . mysql_error());
-    }
-    
-    if($result->num_rows!=0){
-        die(json_encode(['taken'=>'true','email'=>'notSent'])); 
-    }
-    
-    
-    $sql="Insert into user (email, status, verification_code, password, salt) values ('" . $email . "','not','" . $activation . "','" . $password . "','" . $salt . "');";
+    die($response);
+}
 
-    
-    $result = mysqli_query($mysqli,$sql);
-    
-    
-    if (!$result) {
-        die('Invalid query: ' . mysql_error());
-    }
+// Valid input
+require '/home/robert/config/conn.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    
-    $to      = $email;
-    $subject = 'Critter Sitter Activation number!';
-    $message = 'Your activation number is ' . $activation;
-    $headers = 'From: CritterSitter' . "\r\n" .
-    'Reply-To: NOREPLY@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$email = htmlspecialchars($_POST['email']);
+$password = htmlspecialchars($_POST['password']);
+$salt = uniqid(mt_rand(), false);
+$activation = strtoupper(substr(hash("sha256",time(),false),0,5));
 
-    if(mail($to, $subject, $message, $headers)){
-        echo json_encode(['taken'=>'false','email'=>'sent']);
-    }else{
-        echo json_encode(['taken'=>'false','email'=>'notSent']);
-    }
+// Check for email query
+$sql="select * from user where email = '" . $email . "';";
+
+//echo $sql;
+$result = mysqli_query($mysqli,$sql);
+
+//echo mysql_error();
+if (!$result) {
+    echo 'Invalid query: ' . mysql_error();
+
+    $response['message'] = "Creation Unsuccessful!";
+    die(json_encode($response));
+}
+
+// Check if email is already take
+if($result->num_rows != 0) {
+    $response['message'] = "Email already taken!";
+    die(json_encode($response));
+}
+
+// Salt and hash of password
+$saltedPassword = hash("md5", $password.$salt);
+
+// SQL for insertion
+$sql="Insert into user (email, status, verification_code, password, salt) values ('" . $email . "','not','" . $activation . "','" . $saltedPassword . "','" . $salt . "');";
+
+// Parse result
+$result = mysqli_query($mysqli,$sql);
+
+// Check for succes insert
+if (!$result) {
+    echo 'Invalid query: ' . mysql_error();
+
+    $response['errorMessage'] = "Creation Unsuccessful!";
+
+    die(json_encode($response));
+}
+
+// Verification email setup
+$to      = $email;
+$subject = 'Critter Sitter Activation number!';
+$message = 'Your activation number is ' . $activation;
+$headers = 'From: CritterSitter' . "\r\n" .
+'Reply-To: NOREPLY@example.com' . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+if(mail($to, $subject, $message, $headers)) {
+    $response['status'] = "ok";
+    $response['message'] = "Successful account creation!";
+
+    echo json_encode($response);
+} else {
+    $response["message"] = "Failed to send to email account provided!"
+    echo json_encode($response);
 }
 ?>
