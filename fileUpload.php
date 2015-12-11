e017f5a147745c46710d07914a55c3787764c05f
Robert <rochovian@gmail.com>
2015-12-11 14:41:22 -0600

    asdas

--- fileUpload.php
+++ fileUpload.php
@@ -4,34 +4,16 @@
         header('Content-Type: application/json');
         // response from this script is going to be JSON
 
-        // http://stackoverflow.com/questions/26335656/how-to-upload-images-to-a-server-in-ios-with-swift
-        // http://www.w3schools.com/php/php_file_upload.asp
-
-        // On the server you must be sure your script has the rights
-        // to write a file into $destinationDirectory.
-
-
         if($_POST['type'] != 'pet' && $_POST['type'] != 'activity'){
             $response['message'] = "'type' must be set to 'pet' or 'activity'";
             die(json_encode($response));
         }
 
         $fileFormName = 'file';
-        $destinationDirectory = "/home/robert/coolsite/test/" . $_POST['type'];
-
-        // The rights you set when you create the directory determine
-        // who can write to it and who can read from it.  Be sure you 
-        // are setting the rights that are safe and meet your purpose.
-        // What you can do depends on how your web server is set up.
-        // Don't just copy my code!!!
-//        echo $destinationDirectory;
-//        echo file_exists($destinationDirectory);
-        $response['currentUser'] = get_current_user();
-	 $response['directory'] = $destinationDirectory;
-        $response['fileExists'] = file_exists($destinationDirectory);
+        $destinationDirectory = "/home/robert/pics/" . $_POST['type'];
+        
         if (!file_exists($destinationDirectory)) {
             mkdir($destinationDirectory, 0755, true);
-            echo "hey";
         }
 
         $tempSourceFilePath = $_FILES[$fileFormName]['tmp_name'];
@@ -46,11 +28,8 @@
             $fileMimeType = $check['mime'];
         } else {
             // is not a real image - send error and exit! - do not copy file
-            print json_encode([
-                'status' => 'ERROR',
-                'message' => $fileName . ' is not an image!'
-            ]);
-            exit;
+            $response['message'] = "Not an image!";
+            die json_encode($response);
         }
 
         if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
@@ -59,17 +38,12 @@
             $protocol = 'https://';
         }
         $baseURL = $protocol . $_SERVER['SERVER_NAME'] . '/coolsite/test';
-        //dirname($_SERVER['PHP_SELF'])
-        $response['baseUrl'] = $baseURL;
+        
         if (move_uploaded_file($tempSourceFilePath, $destinationPath)) {
-            print json_encode([
-                'status' => 'OK',
-                'message' => 'Uploaded ' . $fileName,
-                'fileExtension' => $fileExtension,
-                'fileMimeType' => $fileMimeType,
-                'relativeURL' => $destinationDirectory . '/' . $fileName,
-                'URL' => $baseURL . '/' . $destinationDirectory . '/' . $fileName
-            ]);
+            $response['status'] = 'ok';
+            $response['message'] = 'Uploaded ' . $fileName . $fileExtension;
+            $response['destination'] = $destinationDirectory . '/' . $fileName
+            print json_encode($response);
         } else {
             $response['message'] = 'An error occurred uploading ' . $fileName;
             print json_encode($response);
