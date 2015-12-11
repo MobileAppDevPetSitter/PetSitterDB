b41c37e80928688c01bb4105b7bedf80f9cdc687
Robert Stovall <Robert@MUs-MacBook-Pro.centurylink.net>
2015-12-10 22:00:26 -0600

    maybe not

--- sittingDeletion.php
+++ sittingDeletion.php
@@ -8,12 +8,12 @@
         // Check for email query
         $sql="Delete from pet_sitting where pet_sitting_id = '" . $sitting_id . "');";
 
-//        echo $sql;
+        echo $sql;
         $result = mysqli_query($mysqli,$sql);
 
         //echo mysql_error();
         if (!$result) {
-            $response['message'] = "Query Failed" . mysqli_error($result);
+            $response['message'] = "Query Failed" . mysqli_error();
             die(json_encode($response));
         } else {
             $response['status'] = 'ok';
