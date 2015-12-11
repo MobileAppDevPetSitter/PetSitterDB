a14dbe8f969a1c4c24ae096d796fa2669c5ddec1
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-12-10 18:47:03 -0600

    Added creation of owner and user together

--- petCreate.php
+++ petCreate.php
@@ -1,7 +1,7 @@
 <?php
     require '/home/robert/config/conn.php';
 	$response["status"] = "bad";
-    if(isset($_POST['name']) && isset($_POST['bio']) && isset($_POST['medicine']) && isset($_POST['bathroom']) && isset($_POST['exercise']) && isset($_POST['emergency_contact']) && isset($_POST['veterinarian']) && isset($_POST['other'])){
+    if(isset($_POST['user_id']) && isset($_POST['name']) && isset($_POST['bio']) && isset($_POST['medicine']) && isset($_POST['bathroom']) && isset($_POST['exercise']) && isset($_POST['emergency_contact']) && isset($_POST['veterinarian']) && isset($_POST['other'])){
         
 	$pet_medicine   = htmlspecialchars($_POST['medicine']);
         $pet_name       = htmlspecialchars($_POST['name']);
@@ -11,6 +11,7 @@
         $pet_veterinarian_info = htmlspecialchars($_POST['veterinarian']);
         $pet_emergency  = htmlspecialchars($_POST['emergency_contact']);
         $pet_other      = htmlspecialchars($_POST['other']);
+        $owner_id       = htmlspecialchars($_POST['user_id']);
 
         // Check for email query
         $sql="INSERT into pet (name,bio,bathroom_instructions,medicine,exercise_instructions,veterinarian_info,emergency_contact,other) 
@@ -18,22 +19,31 @@
 
         //echo $sql;
         $result = mysqli_query($mysqli,$sql);
+
         $response["status"] = "bad";
         //echo mysql_error();
         if (!$result) {
             $response['message'] = "Query Failed" . mysqli_error();
             die(json_encode($response));
         } else {
-            $response["id"]      = mysqli_insert_id($mysqli);
-            $response['status']  = "ok";
-            $response['message'] = "Pet created";
-            print json_encode($response);
+            $response['id']      = mysqli_insert_id($mysqli);
+
+            $owner_sql = "INSERT into  owner (owner_id, pet_id) VALUES ('" . $owner_id . "','" . $response['id']  . "');";
+
+            $result = mysqli_query($mysqli,$sql);
+            
+            if(!$result) {
+                $response['status']  = "ok";
+                $response['message'] = "Pet created";
+                print json_encode($response);  
+            } else {
+                $response['message'] = "Could not add user as pet owner";
+                print json_encode($response);      
+            }
         }    
-    } else {
-        
+    } else {    
         $response['message'] =  "Must set 'name', 'bio', 'bathroom', 'exercise', 'emergency_contact','veterinarian_info','other'";
         print json_encode($response);
-        
     }
 
     $mysqli->close();
