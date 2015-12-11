e6e54c7d9006263d42aa7a5e702691ad43c3081e
Robert Stovall <Robert@MUs-MacBook-Pro.centurylink.net>
2015-12-10 21:28:20 -0600

    asd

--- sittingCreate.php
+++ sittingCreate.php
@@ -9,32 +9,43 @@
         
         $result = mysqli_query($mysqli,$sql);
         
-        
-        $sitter      = htmlspecialchars($_POST['sitter_id']);                
-        $pet         = htmlspecialchars($_POST['pet_id']);
-        $start       = htmlspecialchars($_POST['start_date']);
-        $end         = htmlspecialchars($_POST['end_date']);
-
-        // Check for email query
-        $sql="INSERT into pet_sitting (sitter_id,pet_id,status,start_date,end_date) 
-              VALUES ('" . $sitter . "','" . $pet . "','open','" . $start . "','" . $end . "');";
-
-//        echo $sql;
-        $result = mysqli_query($mysqli,$sql);
-        //echo mysql_error();
-        if (!$result) {
-            $response['message'] = "Query Failed" . mysqli_error();
-            die(json_encode($response));
-        } else {
+        if ($result) {
             
-            $response['message'] = "Sitting instance created";
-            $response['status']  = "ok";
-            print json_encode($response);
+            $query       = mysqli_fetch_assoc($result);
+            echo $query;
+            $sitter      = htmlspecialchars($query['id']);                
+            $pet         = htmlspecialchars($_POST['pet_id']);
+            $start       = htmlspecialchars($_POST['start_date']);
+            $end         = htmlspecialchars($_POST['end_date']);
+
+            // Check for email query
+            $sql="INSERT into pet_sitting (sitter_id,pet_id,status,start_date,end_date) 
+                  VALUES ('" . $sitter . "','" . $pet . "','open','" . $start . "','" . $end . "');";
+
+    //        echo $sql;
+            $result = mysqli_query($mysqli,$sql);
+            //echo mysql_error();
+            if (!$result) {
+                $response['message'] = "Query Failed" . mysqli_error();
+                die(json_encode($response));
+            } else {
+
+                $response['message'] = "Sitting instance created";
+                $response['status']  = "ok";
+                print json_encode($response);
+            }
+        
+            
+            
+        } else {
+            $response['message'] = "Email not found" . mysqli_error();
+            die(json_encode($response));
         }
         
-    } else {
         
-        print "Must set 'email','pet_id','start_date','end_date'";
+    } else {
+        $response['message'] = "Must set 'email','pet_id','start_date','end_date'";
+        die(json_encode($response));
         
     }
 
