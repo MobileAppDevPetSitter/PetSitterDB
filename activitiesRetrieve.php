e27d033b7232e3c197a0b20679075d3d7f491e56
Robert <rochovian@gmail.com>
2015-12-14 23:11:50 -0600

    fixing

---
+++ activitiesRetrieve.php
@@ -0,0 +1,43 @@
+<?php
+    require '/home/robert/config/conn.php';
+    $response['status'] = 'bad';
+    if(isset($_POST['pet_sitting_id'])){
+        
+        $pet_sitting_id = htmlspecialchars($_POST['pet_sitting_id']);
+        
+        $sql="select * from activity where pet_sitting_id = '" . $pet_sitting_id . "';";
+        
+        $result = mysqli_query($mysqli,$sql);
+
+        //select pets owned
+        
+        if (!$result) {
+            $response['message'] = "Query Failed" . mysql_error();
+            die(json_encode($response));
+        } else {
+            
+            if($result->num_rows >0){
+                $counter = 0;
+                while($row = mysqli_fetch_assoc($result)){
+                        
+                        $activity    = mysqli_fetch_assoc($activityResult);
+                        $response['status'] = 'ok';
+                        $activities[$counter] = $activity;
+                        $response['activities'] = $activities;
+                        $counter++;
+                }
+            } else {
+                $response['message'] = "Instance has no activities";
+            }
+            
+        }
+        
+        
+    $mysqli->close();
+    print json_encode($response);
+        
+    } else {
+        print "must set 'pet_sitting_id'";
+    }
+
+?>
