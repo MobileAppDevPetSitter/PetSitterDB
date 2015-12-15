29924aa93641a6216535764256d2f1c4b4890f45
Robert <rochovian@gmail.com>
2015-12-14 21:55:31 -0600

    fixing

--- ownedPetsRetrieval.php
+++ ownedPetsRetrieval.php
@@ -42,6 +42,7 @@
                         $pet['other']    = $petArray['other'];
                         $pet['medicine'] = $petArray['medicine'];
                         $pet['vet']      = $petArray['veterinarian_info'];
+                        $pet['hasImage'] = $petArray['hasImage'];
                         $response['status'] = 'ok';
                         $pets[$counter] = $pet;
                         $response['pets'] = $pets;
@@ -54,56 +55,101 @@
             
         }
         
-        //selecting pet instances of pets owned
+        //selecting pet_sitting instances of pets owned
         
         $counter = 0;
         foreach($coolPets as $coolPetId){
-        
-            $sql="select * from pet_sitting where pet_id = '" . $coolPetId . "';";
+           $sql="select * from pet_sitting where pet_id =" $coolPetId . "';";
 
+    //        echo $sql;
             $result = mysqli_query($mysqli,$sql);
 
+            //echo mysql_error();
             if (!$result) {
-                $response['message'] = "selecting from pet_sitting Failed" . mysql_error();
+                $response['message'] = "Query Failed" . mysql_error();
                 die(json_encode($response));
             } else {
+                $response["status"] = "ok";
 
-                if($result->num_rows >0){
-                    while($row = mysqli_fetch_assoc($result)){
+                $pet = mysqli_fetch_assoc($result);
 
-                        $sql="select * from pet where pet_id = '" . $coolPetId . "';";
+                $response['sitter_id']   = $pet['sitter_id'];
+                $response['pet_id']      = $pet['pet_id'];
+                $response['currentStatus']      = $pet['status'];
+                $response['start']       = $pet['start_date'];
+                $response['end']         = $pet['end_date'];
 
-                        $petResult = mysqli_query($mysqli,$sql);
+                $currentDate = date("Y-m-d H:i:s");
 
-                        if (!$petResult) {
-                            $response['message'] = "query 5 didn't work" . mysql_error();
-                            die(json_encode($response));
-                        } else {
-
-                            $petArray     = mysqli_fetch_assoc($petResult);
-
-                            $pet['pet_id']   = $petArray['pet_id'];
-                            $pet['name']     = $petArray['name'];
-                            $pet['bio']      = $petArray['bio'];
-                            $pet['bathroom'] = $petArray['bathroom_instructions'];
-                            $pet['exercise'] = $petArray['exercise_instructions'];
-                            $pet['food']     = $petArray['food'];
-                            $pet['emergency'] = $petArray['emergency_contact'];
-                            $pet['other']    = $petArray['other'];
-                            $pet['medicine'] = $petArray['medicine'];
-                            $pet['vet']      = $petArray['veterinarian_info'];
-                            $response['status'] = 'ok';
-                            $sittingPets[$counter] = $pet;
-                            $response['mine_sitting'] = $sittingPets;
-                            $counter++;
-                        }
-                    }
-                } else {
-                    $response['message'] = "User's pets aren't being sit";
+                if($response['start'] < $currentDate && $response['end'] > $currentDate ) {
+                    $response['currentStatus'] = 'current';
+                } else if ($currentDate < $response['start']){
+                    $response['currentStatus'] = 'upcoming';
+                } else if ($currentDate > $response['end']){
+                    $response['currentStatus'] = 'expired';
                 }
+
+                $sql="UPDATE pet_sitting
+                  SET status = '"     . $response['currentStatus'] . 
+                  "' WHERE pet_sitting_id = '" . $sitting_id . "';";
+
+                if (!$result) {
+                    $response['status'] = 'bad';
+                    $response['message'] = "Status update failed" . mysqli_error();
+                    die(json_encode($response));
+                } else {
+                    $response['status']  = 'ok';
+                    $response['message'] = "Status updated";
+                }
+
+
+                print json_encode($response);
+            }
+//            $sql="select * from pet_sitting where pet_id = '" . $coolPetId . "';";
+//
+//            $result = mysqli_query($mysqli,$sql);
+//
+//            if (!$result) {
+//                $response['message'] = "selecting from pet_sitting Failed" . mysql_error();
+//                die(json_encode($response));
+//            } else {
+//
+//                if($result->num_rows >0){
+//                    while($row = mysqli_fetch_assoc($result)){
+//
+//                        $sql="select * from pet where pet_id = '" . $coolPetId . "';";
+//
+//                        $petResult = mysqli_query($mysqli,$sql);
+//
+//                        if (!$petResult) {
+//                            $response['message'] = "query 5 didn't work" . mysql_error();
+//                            die(json_encode($response));
+//                        } else {
+//
+//                            $petArray     = mysqli_fetch_assoc($petResult);
+//
+//                            $pet['pet_id']   = $petArray['pet_id'];
+//                            $pet['name']     = $petArray['name'];
+//                            $pet['bio']      = $petArray['bio'];
+//                            $pet['bathroom'] = $petArray['bathroom_instructions'];
+//                            $pet['exercise'] = $petArray['exercise_instructions'];
+//                            $pet['food']     = $petArray['food'];
+//                            $pet['emergency'] = $petArray['emergency_contact'];
+//                            $pet['other']    = $petArray['other'];
+//                            $pet['medicine'] = $petArray['medicine'];
+//                            $pet['vet']      = $petArray['veterinarian_info'];
+//                            $pet['hasImage'] = $petArray['hasImage'];
+//                            $response['status'] = 'ok';
+//                            $sittingPets[$counter] = $pet;
+//                            $response['mine_sitting'] = $sittingPets;
+//                            $counter++;
+//                        }
+//                    }
+//                } else {
+//                    $response['message'] = "User's pets aren't being sit";
+//                }
             
         }
-    }
         
         
     $mysqli->close();
